329b756de4faad08628af0e7deb5be408fc50883
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-30 12:51:57 +0600

    Inheritance part-2. Parent

---
+++ PHP-OOP-Repeat/08/classes/BookProduct.php
@@ -0,0 +1,26 @@
+<?php
+
+class BookProduct extends Product
+{
+
+    public $numPages;
+
+    public function __construct($name, $price, $numPages)
+    {
+        parent::__construct($name, $price);
+        $this->numPages = $numPages;
+    }
+
+    public function getProduct()
+    {
+        $out = parent::getProduct();
+        $out .= "NumPages: {$this->numPages}<br>";
+        return $out;
+    }
+
+    public function getNumPages()
+    {
+        return $this->numPages;
+    }
+
+}
